2aa659b230618a42bbd2bad24037e399b000f010
Tamas Nemeth <nemeth.t91@gmail.com>
2023-10-25 21:57:02 +0100

    Add function for label updating as a property

--- timer.py
+++ timer.py
@@ -1,22 +1,23 @@
 import tkinter as tk
+from settings import TIMER_LENGTH
+from typing import Callable
 
 
 class Timer:
-    def __init__(self, window: tk.Tk, canvas: tk.Canvas, timer_text):
+    def __init__(self, window: tk.Tk, label_update_func: Callable):
         self.window = window
-        self.canvas = canvas
-        self.timer_text = timer_text
-        self.timer_count = 60
+        self.label_update = label_update_func
+        self.timer_count = TIMER_LENGTH
         self.timer = None
 
-    def start_timer(self, timer_length=60):
+    def start_timer(self, timer_length=TIMER_LENGTH):
         self.timer_count = timer_length
         self.count_down(self.timer_count)
 
     def count_down(self, count):
         if count >= 0:
             self.timer_count = count
-            self.canvas.itemconfig(self.timer_text, text=self.timer_count)
+            self.label_update(self.timer_count)
             self.timer = self.window.after(1000, self.count_down, self.timer_count - 1)
         else:
             self.window.after_cancel(self.timer)
