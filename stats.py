8d58abfa719654c016d1906a6ec48012a848e1db
Tamas Nemeth <nemeth.t91@gmail.com>
2023-10-25 15:32:41 +0100

    Modify Statistics class: combine word and letter stats together

--- stats.py
+++ stats.py
@@ -1,48 +1,37 @@
-class Stat:
-    def __init__(self, text: str, is_correct: bool):
-        self.text = text
+class WordStat:
+    def __init__(self, word: str, is_correct: bool, correct_characters: int):
+        self.word = word
         self.is_correct = is_correct
+        self.correct_characters = correct_characters
 
 
 class Statistics:
     def __init__(self):
-        self.letters: list[Stat] = []
-        self.words: list[Stat] = []
+        self.word_stats: list[WordStat] = []
 
-    def add_letters(self, letter: str, is_correct: bool):
-        new_letter_stat = Stat(letter, is_correct)
-        self.letters.append(new_letter_stat)
+    def add_words(self, word: str, is_correct: bool, correct_characters: int):
+        new_word_stat = WordStat(word, is_correct, correct_characters)
+        self.word_stats.append(new_word_stat)
 
-    def add_words(self, word: str, is_correct: bool):
-        new_word_stat = Stat(word, is_correct)
-        self.words.append(new_word_stat)
-
-    def remove_last_letter(self):
-        self.letters.pop()
-
-    def remove_last_word(self):
-        self.words.pop()
-
-    def get_letter_count(self):
-        return len(self.letters)
-
-    def get_correct_letter_count(self):
-        return self.count_correct(self.letters)
+    def get_char_count(self):
+        if not self.word_stats:
+            return 0
+        char_counts = [len(stat.word) for stat in self.word_stats]
+        return sum(char_counts)
 
     def get_word_count(self):
-        return len(self.letters)
+        return len(self.word_stats)
 
-    def get_correct_word_count(self):
-        return self.count_correct(self.words)
-
-    @staticmethod
-    def count_correct(stat_list: list[Stat]):
-        if not stat_list:
+    def get_correct_words_count(self):
+        if not self.word_stats:
             return 0
-        count = 0
-        for stat in stat_list:
-            if stat.is_correct:
-                count += 1
-        return count
+        correct_words = [stat for stat in self.word_stats if stat.is_correct]
+        return len(correct_words)
+
+    def get_correct_char_count(self):
+        if not self.word_stats:
+            return 0
+        correct_chars = [stat.correct_characters for stat in self.word_stats]
+        return sum(correct_chars)
 
 
